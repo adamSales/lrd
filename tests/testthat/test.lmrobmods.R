603e300f0db9af4e5d48ada87aad85b255bbe458
Ben Hansen <bbh@umich.edu>
2016-09-23 18:12:54 -0400

    a little more on tests
    intermediate commit, uncovered small problem in testing file but haven't yet fixed

--- tests/testthat/test.lmrobmods.R
+++ tests/testthat/test.lmrobmods.R
@@ -1,7 +1,8 @@
 context("Non-standard uses of robustbase's lmrob and helpers")
 
-test_that("Proper use of .vcov.avar1()",{
-     data(coleman)
+test_that("On-label use of .vcov.avar1()",{
+    expect_true(require('robustbase'))
+    data(coleman)
      set.seed(0)
      m1 <- lmrob(Y ~ ., data=coleman) 
      expect_equivalent(robustbase:::.vcov.avar1(m1, m1$x), m1$cov)
@@ -12,9 +13,23 @@ test_that("Proper use of .vcov.avar1()",{
               residuals = init.S$resid)
      }
      set.seed(0)
-     m2 <- lmrob(Y ~ ., data=coleman, method = "M", init = initFun1)
+    m2 <- lmrob(Y ~ ., data=coleman, method = "M", init = initFun1)
+    expect_false( all.equal(m1$cov, # the two cov estimation methods could give the same answer just 
+                            m2$cov, # by chance; confirming it's not happening in this example
+                            check.attributes = FALSE) )
      expect_error(robustbase:::.vcov.avar1(m2, m2$x), "supports only SM or MM estimates")
      m3 <- m2
      m3$control$method <- "SM" # since it really is an SM estimate
-     expect_equivalent(robustbase:::.vcov.avar1(m3, m3$x), m1$cov)
+    expect_equivalent(robustbase:::.vcov.avar1(m3, m3$x), m1$cov)
+    m4 <- lmrob(Y ~ ., data=coleman, method = "M", init = m2$init)
+    expect_error(robustbase:::.vcov.avar1(m4, m4$x), "supports only SM or MM estimates")
+    m5 <- m4
+    m5$control$method <- "SM"
+    expect_equivalent(robustbase:::.vcov.avar1(m5, m5$x), m1$cov)
    })
+
+test_that("Off-label use of .vcov.avar1()",{
+    expect_true(require('robustbase'))
+    data(coleman)
+    
+          })
