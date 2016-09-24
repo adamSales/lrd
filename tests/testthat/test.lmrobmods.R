272982004912aaf598a9ff907157111e4820c19f
Ben B Hansen <bbh@umich.edu>
2016-09-23 23:38:57 -0400

    document constraints on trickery w/ init functions

--- tests/testthat/test.lmrobmods.R
+++ tests/testthat/test.lmrobmods.R
@@ -5,7 +5,8 @@ test_that("On-label use of .vcov.avar1()",{
     data(coleman)
      set.seed(0)
      m1 <- lmrob(Y ~ ., data=coleman) 
-     expect_equivalent(robustbase:::.vcov.avar1(m1, m1$x), m1$cov)
+    expect_equal(m1$control$cov, ".vcov.avar1")
+    expect_equivalent(robustbase:::.vcov.avar1(m1, m1$x), m1$cov)
      initFun1 <- function(x, y, control, mf) {
          init.S <- lmrob.S(x, y, control)
          list(coefficients=init.S$coef,
@@ -14,9 +15,10 @@ test_that("On-label use of .vcov.avar1()",{
      }
      set.seed(0)
     m2 <- lmrob(Y ~ ., data=coleman, method = "M", init = initFun1)
-    expect_false( all.equal(m1$cov, # the two cov estimation methods could give the same answer just 
+    expect_equal(m2$control$cov, ".vcov.w")
+    expect_false( isTRUE(all.equal(m1$cov, # the two cov estimation methods could give the same answer just 
                             m2$cov, # by chance; confirming it's not happening in this example
-                            check.attributes = FALSE) )
+                            check.attributes = FALSE)) )
      expect_error(robustbase:::.vcov.avar1(m2, m2$x), "supports only SM or MM estimates")
      m3 <- m2
      m3$control$method <- "SM" # since it really is an SM estimate
@@ -31,5 +33,27 @@ test_that("On-label use of .vcov.avar1()",{
 test_that("Off-label use of .vcov.avar1()",{
     expect_true(require('robustbase'))
     data(coleman)
+     set.seed(0)
+     m1 <- lmrob(Y ~ . + I(sstatus>0), data=coleman) 
+     expect_equivalent(robustbase:::.vcov.avar1(m1, m1$x), m1$cov)
+     initFun2 <- function(x, y, control, mf) {
+         init.S <- lmrob.S(x, y, control)
+         list(coefficients=numeric(length(init.S$coef)), # lmrob.fit check that this is present, has an entry for each col of x
+              scale = init.S$scale, 
+              residuals = init.S$resid)
+     }
+    set.seed(0)
+    m2 <- lmrob(Y ~ .  + I(sstatus>0), data=coleman, method = "M", init = initFun2) # works, but...
+    expect_equal(coef(m2), rep(0, length(coef(m2)))) # oops.
+     initFun3 <- function(x, y, control, mf) {
+         xtrunc <- x[,-ncol(x)]
+         init.S <- lmrob.S(xtrunc, y, control)
+         list(coefficients=c(init.S$coef, 0), 
+              scale = init.S$scale, 
+              residuals = init.S$resid)
+     }
+    m3 <- lmrob(Y ~ .  + I(sstatus>0), data=coleman, method = "M", init = initFun3) 
+    m3$control$method <- "SM"
+    robustbase:::.vcov.avar1(m3, m3$x)
     
           })
