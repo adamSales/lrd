e4982c4ad9035ed26d44347aba9295a5f72dbebf
Ben B Hansen <bbh@umich.edu>
2016-10-03 10:56:20 -0400

    Import Krafft point data set

---
+++ R/data.R
@@ -0,0 +1,13 @@
+##' Jalali-Heravi and Knouz's (2002) measurements from an assay aiming
+##' to develop a prediction equation for the "Krafft point", as reported
+##' in Maronna et al (2006), Sec 5.10, "the exact fit property". 
+##'
+##' @source Jalali-Heravi, M. and Knouz, E. (2002), \dQuote{Use of quantitative structure-property relationships in predicting the Krafft point of anionic surfactants.} \emph{Electronic Journal of Molecular Design}, 1, 410-417.
+##' @title Krafft point assay data
+##' @format Data frame with 32 rows and 2 columns
+##' \itemize{
+##'     \item heat
+##'     \item point Krafft point
+##' }
+##' @references Maronna, R. A. R. D., Martin, D. and Yohai, V. (2006), \emph{Robust statistics.} John Wiley and Sons, Chichester.
+"krafft"
