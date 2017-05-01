57dcd48f5df5ba75620797e6f3ec0801309d0868
Ben B Hansen <bbh@umich.edu>
2017-05-01 14:25:05 -0400

    Data help page, w/ basic dictionary

---
+++ R/data.R
@@ -0,0 +1,21 @@
+#' Data from Lindo et al (2010) academic probation study
+#'
+#' Data set with demographics, academic achievement info on
+#' 42 thousand students at 3 unnamed Canadian universities.
+#'
+#' @format A data frame of  42187 rows and 90 variables, including:
+#' \describe{
+#'     \item{left_school} 1 if the student left school
+#'     \item{dist_from_cut} The running variable, i.e. gpa - cutpoint
+#'     \item{gpalscutoff} Whether students 1st year GPA fell to left of cutoff
+#'     \item{lhsgrade_pct}
+#'     \item{totcredits_year1}
+#'     \item{male}
+#'     \item{loc_campus1}
+#'     \item{loc_campus2}
+#'     \item{bpl_north_america}
+#'     \item{english}
+#'     \item{age_at_entry}
+#' }
+#' @ source \url{https://www.aeaweb.org/aej/app/data/2008-0202_data.zip}
+"dat" 
